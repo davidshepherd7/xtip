3ebd45f692986e9aee678fbdf14c070cbfc31628
David Shepherd <davidshepherd7@gmail.com>
2020-05-09 16:22:47 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# X-tip
+
+A semi-clone of tanin47's [tip](https://github.com/tanin47/tip) but for X11 (i.e. Linux).
+
+Alpha stage, anything may change at any time.
+
+Customised in python (but it's very easy to run shell commands from python if
+you prefer another language).
+
+
+## Installation and setup
+
+* Install the core dependencies: `zenity`, `dmenu`, `xclip`
+  * e.g. on Debian-based OS: `sudo apt install zenity xclip suckless-tools`
+* Save x-tip.py into a directory on your `$PATH` and `chmod +x x-tip.py`
+* Bind a hotkey to it using your preferred method. 
+  * I use [sxhkd](https://github.com/baskerville/sxhkd), but I think most
+  desktop environments have a hotkey binding system.
+* Optionally install dependencies for any individual commands that you want to use:
+  * GoogleTranslate requires the python googtrans library
+  * Emacsclient requires emacs (obviously)
+
+
+## Writing your own commands
+
+
+For most customisation of commands you should probably just write your own
+(because it only takes a few lines of python).
+
+To do so: write a new class derived from `Command` and decorate it with `@command`.
+
+TODO: better docs?
+
+
+## TODO
+
+* Write some tests
+* Set up typechecking properly
+* Add support for config files to specify commands
+* Add support for installing using `pip` (and break up into multiple files)
+* Try to display useful outputs in dmenu completion (e.g. converted datetimes)
+* Something better than dmenu? Better mouse support, popup at cursor.
+* Figure out how to construct an absolute path from 
---
+++ x-tip.py
@@ -0,0 +1,170 @@
+#! /usr/bin/env python3
+
+
+import sys
+from subprocess import check_output, check_call, run
+
+from typing import Optional
+
+
+class Quit(Exception):
+    pass
+
+
+def get_x_selection() -> str:
+    # TODO: log stderr?
+    output = check_output(["xclip", "-o"])
+    return output.decode("utf-8")
+
+
+def set_x_selection(text: str) -> None:
+    run(["xclip", "-i"], input=text.encode("utf-8"))
+
+
+def sanitize(text: str) -> str:
+    return text.strip().replace("\n", " ").replace("\t", " ")
+
+
+def display(text: str) -> None:
+    # TODO: log stderr?
+    check_call(
+        ["zenity", "--info", "--width", "600", "--height", "400", "--text", text]
+    )
+
+
+class Command:
+    @property
+    def unique_name(self) -> str:
+        pass
+
+    def run(self, text: str) -> Optional[str]:
+        pass
+
+    def accepts(self, text: str) -> str:
+        return True
+
+
+ALL_COMMANDS = []
+
+
+def command(c: Command):
+    ALL_COMMANDS.append(c())
+
+
+def pick_command(text: str) -> Command:
+    commands = [c for c in ALL_COMMANDS if c.accepts(text)]
+
+    if len(commands) == 0:
+        raise Exception("No possible commands")
+
+    if len(commands) == 1:
+        return commands[0]
+
+    names = "\n".join(c.unique_name for c in commands).encode("utf-8")
+    try:
+        dmenu_result = check_output(
+            ["dmenu", "-p", "select command: ", "-i"], input=names
+        )
+    except Exception:
+        print("Dmenu failed which normally means the user hit escape, quitting")
+        raise Quit()
+
+    selected = dmenu_result.strip().decode("utf-8")
+    return next(c for c in commands if c.unique_name == selected)
+
+
+def main(argv):
+
+    selection = sanitize(get_x_selection())
+    print("Got selection:", selection)
+
+    try:
+        command = pick_command(selection)
+        print("Running command:", command.unique_name)
+        result = command.run(selection)
+        print("Finished sucessfully")
+    except Quit:
+        return 0
+    except Exception as e:
+        display("Command failed: " + str(e))
+        raise
+
+    if result is not None:
+        print("Result was:", result)
+        set_x_selection(result)
+        display(result + "\n\nResult copied to X selection.")
+    else:
+        print("No result")
+
+    return 0
+
+
+########################################
+# Commands
+########################################
+
+
+@command
+class UnixTimestamp(Command):
+    unique_name = "Parse unix timestamp"
+
+    def run(self, text: str) -> Optional[str]:
+        from datetime import datetime
+
+        dt = datetime.utcfromtimestamp(int(text))
+        return dt.strftime("%Y-%m-%d %H:%M:%S") + " UTC"
+
+    def accepts(self, text: str) -> str:
+        return text.isnumeric()
+
+
+@command
+class GoogleTranslate(Command):
+    """Depends on googletrans
+    """
+
+    unique_name = "Google translate"
+
+    def run(self, text: str) -> Optional[str]:
+        from googletrans import Translator
+
+        translator = Translator()
+        r = translator.translate(text)
+        return f"Translated {r.src} to {r.dest}: {r.text}"
+
+    def accepts(self, text: str) -> bool:
+        return not text.isnumeric()
+
+
+@command
+class GoogleSearch(Command):
+    unique_name = "Google search"
+
+    def run(self, text: str) -> Optional[str]:
+        import urllib.parse
+
+        escaped = urllib.parse.quote(text)
+        run(["sensible-browser", f"https://google.com/search?q={escaped}"])
+
+
+@command
+class Emacsclient(Command):
+    unique_name = "Open in emacsclient"
+
+    def run(self, text: str) -> Optional[str]:
+        # TODO(david): Figure out a way to get the absolute path... maybe by
+        # guessing from a few possible prefixes?
+        run(["emacsclient", "-c", "-n", text])
+        return None
+
+    def accepts(self, text: str) -> bool:
+        # TODO: allow relative paths
+        return text.startswith("/") or text.startswith("~")
+
+
+########################################
+
+
+# If this script is run from a shell then run main() and return the result.
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
